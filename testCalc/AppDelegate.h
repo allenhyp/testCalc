ec02464a13f3fdf2c15a2de30323704d9ba973e1
許原溥 <Allen_Hsu@yuan-pus-macbook-pro.local>
2015-12-27 15:46:02 +0800

    Initial Commit

---
+++ testCalc/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  testCalc
+//
+//  Created by Yuan-Pu Hsu on 12/27/15.
+//  Copyright © 2015 Yuan-Pu Hsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ testCalc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testCalc
+//
+//  Created by Yuan-Pu Hsu on 12/27/15.
+//  Copyright © 2015 Yuan-Pu Hsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
